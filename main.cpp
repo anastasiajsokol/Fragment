65c892567fae1385dc93e698495ace0cb2cd9b9f
anastasiajsokol <anastasia.j.sokol@gmail.com>
2023-08-25 12:38:32 -0600

    try new example

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 #include <cstdio>
 
 int main(){
-    for(const Token& token : LexStream("test/example.fl")){
+    for(const Token& token : LexStream("test/example_factorial.fl")){
         std::printf("Token{(%ld, %ld), %s, %s}\n", token.position.line, token.position.index, token.value.c_str(), to_string(token.type).c_str());
     }
     return 0;
