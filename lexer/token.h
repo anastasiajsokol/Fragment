bd4fc92bd8026ec8caad53d120f825492704bcbd
anastasiajsokol <anastasia.j.sokol@gmail.com>
2023-08-24 21:40:13 -0600

    skeleton token definition

--- lexer/token.h
+++ lexer/token.h
@@ -1,6 +1,21 @@
 #ifndef LEXER_TOKEN_H
 #define LEXER_TOKEN_H
 
-struct Token{};
+#include <string>
+
+enum class TokenType {
+    end_of_file
+};
+
+struct TokenPosition {
+    size_t line;
+    size_t index;
+};
+
+struct Token {
+    TokenPosition position;
+    std::string value;
+    TokenType type;
+};
 
 #endif
