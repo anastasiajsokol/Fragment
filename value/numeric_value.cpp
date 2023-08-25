0858f51b05c20df478c027f52713d0268c85b04b
anastasiajsokol <anastasia.j.sokol@gmail.com>
2023-08-24 23:23:04 -0600

    remove silly value system, made too soon

--- value/numeric_value.cpp
+++
@@ -1,8 +0,0 @@
-#include "numeric_value.h"
-
-NumericValue::NumericValue() : value(0.0) {}
-NumericValue::NumericValue(const double &value) : value(value) {}
-
-Value& NumericValue::operator =(const Value& other){
-    
-}
