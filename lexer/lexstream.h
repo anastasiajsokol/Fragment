d3ac413d2e4ba279203111402bad899ae221d458
anastasiajsokol <anastasia.j.sokol@gmail.com>
2023-08-24 20:59:36 -0600

    cleanup lexstream definition

--- lexer/lexstream.h
+++ lexer/lexstream.h
@@ -1,3 +1,12 @@
+/**
+ *      @file lexer/lexstream.h
+ *      @brief defines LexStream class to convert file path into stream of Tokens
+ *      @author Anastasia Sokol
+ * 
+ *      LexStream is implimented as a std::input_iterator, meaning it can only be iterated over once
+ *      to check if the stream is still valid (ie, unread) you can use bool LexStream::is_still_valid() or use the LexStream::operator bool() conversion
+**/
+
 #ifndef LEXER_LEXSTREAM_H
 #define LEXER_LEXSTREAM_H
 
@@ -5,17 +14,50 @@
 
 #include <iterator>
 #include <ios>
+#include <memory>
+
+typedef std::unique_ptr<std::FILE, int(*)(FILE*)> unique_file_ptr;  // used to represent std::FILE* as a std::unqiue_ptr
+typedef const char* zstring;                                        // used to represent null terminated c style string (refer to c++ core guidlines for why)
 
 class LexStream {
     private:
-        std::FILE *source;
+        unique_file_ptr source;
+        zstring filepath;
 
     public:
-        struct LexStreamIterator;
+        struct LexStreamDoubleReadException : public std::runtime_error {
+            LexStreamDoubleReadException(const std::string&);
+        };
 
-        LexStream(const char* filepath) throw(std::ios_base::failure);
+        struct LexStreamIterator {
+            private:
+                unique_file_ptr input;
+
+            public:
+                using iterator_category = std::input_iterator_tag;
+                using difference_type   = std::ptrdiff_t;
+                using value_type        = Token;
+                using pointer           = value_type*;
+                using reference         = value_type&;
+
+                LexStreamIterator(unique_file_ptr input) throw(LexStreamDoubleReadException);
+                
+                LexStreamIterator& operator ++() noexcept;
+                
+                const Token *operator ->();
+                const Token *operator *();
+                
+                bool operator ==(const Token&) const noexcept;
+                bool operator !=(const Token&) const noexcept;
+        };
         
-        Token next();
+        LexStream(zstring const filepath) throw(std::ios_base::failure);
+        
+        LexStreamIterator begin() throw(LexStreamDoubleReadException);
+        Token end() const;
+
+        bool is_still_valid() const;
+        operator bool() const;
 };
 
 #endif
