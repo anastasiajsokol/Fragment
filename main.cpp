c3c79781c12fc31f2867eeb72b98033ab74eb226
anastasiajsokol <anastasia.j.sokol@gmail.com>
2023-08-24 23:45:39 -0600

    switch to more idiomatic usage

--- main.cpp
+++ main.cpp
@@ -3,8 +3,7 @@
 #include <cstdio>
 
 int main(){
-    LexStream stream("test/example.fl");
-    for(const Token& token : stream){
+    for(const Token& token : LexStream("test/example.fl")){
         std::printf("Token{(%ld, %ld), %s, test}\n", token.position.line, token.position.index, token.value.c_str());
     }
     return 0;
