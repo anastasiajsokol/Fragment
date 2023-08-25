bab54ac429158d46f4f00f9e6610ba28776e4c59
anastasiajsokol <anastasia.j.sokol@gmail.com>
2023-08-24 23:21:00 -0600

    test mock setup

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
-#include <stdio.h>
+#include "lexer/lexstream.hpp"
+
+#include <cstdio>
 
 int main(){
-    puts("Hey!");
+    LexStream stream("test/example.fl");
+    for(const Token& token : stream){
+        std::printf("Token{(%d, %d), %s, test}\n", token.position.line, token.position.index, token.value.c_str());
+    }
     return 0;
 }
