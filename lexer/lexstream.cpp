e8f9b8dcb5eeac0ac3575844288b31515d538723
anastasiajsokol <anastasia.j.sokol@gmail.com>
2023-08-24 23:44:12 -0600

    use starting position

--- lexer/lexstream.cpp
+++ lexer/lexstream.cpp
@@ -12,7 +12,8 @@ using std::string_literals::operator ""s;
  *  Implimentation of class LexStream
 **/
 
-LexStream::LexStream(zstring const filepath) noexcept(false) : source(unique_file_ptr(std::fopen(filepath, "r"), std::fclose)) {    
+LexStream::LexStream(zstring const filepath) noexcept(false) : source(unique_file_ptr(std::fopen(filepath, "r"), std::fclose)) {
+    // if std::fopen returned nullptr, report error and cancle construction
     if(!source){
         throw std::ios_base::failure("Unable to open file for reading: "s + filepath);
     }
@@ -23,6 +24,7 @@ LexStream::LexStreamIterator LexStream::begin() noexcept(false) {
 }
 
 Token LexStream::end() const noexcept {
+    // sentenial token
     return Token{TokenPosition{-1, -1}, "End of File", TokenType::end_of_file};
 }
 
@@ -31,21 +33,39 @@ Token LexStream::end() const noexcept {
 **/
 
 LexStream::LexStreamIterator::LexStreamIterator(unique_file_ptr input) noexcept(false) : input(std::move(input)) {
+    // check if input == nullptr
     if(!this->input){
         throw LexStreamDoubleReadException("Detected attempt to iterate over invalidated LexStream instance, instances are read once!");
     }
+
+    // set cursor to first token
+    ++*this;
 }
 
 LexStream::LexStreamIterator& LexStream::LexStreamIterator::operator ++() noexcept {
+    // peek ahead to next value
+    const auto peek = [](std::FILE* stream) -> int { int value = getc(stream); ungetc(value, stream); return value; };
+
+    // wrapper around getc that also updates position
+    const auto read = [this](std::FILE* stream) -> int {
+        int value = getc(stream);
+        if(value == '\n'){
+            ++this->position.line;
+            this->position.index = 0;
+        } else {
+            ++this->position.index;
+        }
+        return value;
+    };
+
     // read next possible token (note: may be invalid!) from file
-    const auto next = [](std::FILE* stream) -> std::optional<std::string> {
+    const auto next = [peek, read](std::FILE* stream) -> std::optional<std::string> {
         // values that evaluate to true signify an end to reading
-        const auto is_token_terminator = [](int character) -> bool { return (character == EOF) || std::isspace(character) || (character == '(' || character == ')'); };
+        const auto is_token_terminator = [](int character) -> bool { return (character == EOF) || !character || std::isspace(character) || (character == '(' || character == ')'); };
 
         // clear whitespace
-        int ch = getc(stream);
-        while(ch != EOF && std::isspace(ch)){ ch = getc(stream); }
-        ungetc(ch, stream);
+        int ch = read(stream);
+        while(ch != EOF && std::isspace(ch)){ ch = read(stream); }
 
         if(ch == EOF){
             return std::nullopt;
@@ -54,14 +74,23 @@ LexStream::LexStreamIterator& LexStream::LexStreamIterator::operator ++() noexce
         // read till terminator
         std::string sequence(1, ch);
 
-        while(!is_token_terminator(ch)){
-            sequence += ch = getc(stream);
+        if(is_token_terminator(ch)){
+            return sequence;
+        }
+
+        while(!is_token_terminator(peek(stream))){
+            sequence += ch = read(stream);
         }
 
         return sequence;
     };
 
+    // copy start of token (reporting start position over end is just slightly cleaner in my opinion)
+    TokenPosition start_position = position;
+    
+    // return token, or return sentenial to signal end of file
     std::optional<std::string> token = next(input.get());
-    cursor = token.has_value() ? Token{position, token.value(), TokenType::test} : Token{position, "End of File", TokenType::end_of_file};
+    cursor = token.has_value() ? Token{start_position, token.value(), TokenType::test} : Token{position, "End of File", TokenType::end_of_file};
+    
     return *this;
 }
