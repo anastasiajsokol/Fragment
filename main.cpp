2fcf67d09b9d7fdc122b56d79be52b94dfbff7b4
anastasiajsokol <anastasia.j.sokol@gmail.com>
2023-08-24 23:23:56 -0600

    fix position format specifiers

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 int main(){
     LexStream stream("test/example.fl");
     for(const Token& token : stream){
-        std::printf("Token{(%d, %d), %s, test}\n", token.position.line, token.position.index, token.value.c_str());
+        std::printf("Token{(%ld, %ld), %s, test}\n", token.position.line, token.position.index, token.value.c_str());
     }
     return 0;
 }
