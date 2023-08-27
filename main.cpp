fd859df0b7f87721ccf90c55ff553859b58d9ad2
anastasiajsokol <anastasia.j.sokol@gmail.com>
2023-08-27 14:54:27 -0600

    add include and file comments

--- main.cpp
+++ main.cpp
@@ -1,9 +1,18 @@
-#include "lexer/invalidlexeme.hpp"
-#include "lexer/lexstream.hpp"
-#include "parser/blockstream.hpp"
-#include "parser/invalidblock.hpp"
+/**
+ *      @file main.cpp
+ *      @brief defines entry point for generic use
+ *      @author Anastasia Sokol
+ *      @copyright 2023 Anastasia Sokol | Released under The Unlicense
+**/
 
-#include <cstdio>
+#include "lexer/invalidlexeme.hpp"  // defines lexer::InvalidLexeme for reporting generic lexing errors
+#include "lexer/lexstream.hpp"      // defines lexer::LexStream for creating token streams from file path
+#include "parser/blockstream.hpp"   // defines parser::BlockStream for creating block streams from token streams
+#include "parser/invalidblock.hpp"  // defines parser::InvalidBlock for reporting generic block parsing errors
+
+#include <ios>                      // defines std::ios_base::failure for file io errors (also defined in lexer/lexstream.hpp but that is not generally guaranteed)
+
+#include <cstdio>                   // defines std::fprintf, stderr, EXIT_FAILURE, and EXIT_SUCCESS for reporting program execution state
 
 int main(){
     const char* filepath = "test/example_factorial.fl";
@@ -13,12 +22,15 @@ int main(){
             std::printf("Block with size: %ld\n", block.size());
         }
     } catch(std::ios_base::failure error){
-        std::printf("File Error\n\t%s\n", error.what());
+        std::fprintf(stderr, "File Error\n\t%s\n", error.what());
+        return EXIT_FAILURE;
     } catch(lexer::InvalidLexeme error) {
-        std::printf("Invalid Lexeme Exception\n\terror: %s\n\tposition: (%ld, %ld) in file %s\n", error.what(), error.position.line, error.position.index, filepath);
+        std::fprintf(stderr, "Invalid Lexeme Exception\n\terror: %s\n\tposition: (%ld, %ld) in file %s\n", error.what(), error.position.line, error.position.index, filepath);
+        return EXIT_FAILURE;
     } catch(parser::InvalidBlock error) {
-        std::printf("Invalid Block Exception\n\terror: %s\n\tposition: (%ld, %ld) in file %s\n", error.what(), error.position.line, error.position.index, filepath);
+        std::fprintf(stderr, "Invalid Block Exception\n\terror: %s\n\tposition: (%ld, %ld) in file %s\n", error.what(), error.position.line, error.position.index, filepath);
+        return EXIT_FAILURE;
     }
 
-    return 0;
+    return EXIT_SUCCESS;
 }
