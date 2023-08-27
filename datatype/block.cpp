d62824df581a4605857c0ca022d7158a0c70e487
anastasiajsokol <anastasia.j.sokol@gmail.com>
2023-08-26 22:44:46 -0600

    add noexcept modifier

--- datatype/block.cpp
+++ datatype/block.cpp
@@ -1,13 +1,13 @@
 #include "block.h"
 
-void Block::append(std::variant<Token, Block> value){
+void Block::append(std::variant<Token, Block> value) {
     view.push_back(value);
 }
 
-Block::list_iterator_begin_type Block::begin(){
+Block::list_iterator_begin_type Block::begin() noexcept {
     return view.begin();
 }
 
-Block::list_iterator_end_type Block::end(){
+Block::list_iterator_end_type Block::end() noexcept {
     return view.end();
 }
