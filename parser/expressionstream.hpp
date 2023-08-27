9a81f2fd1e6902d399fd7ba6cb635b7bc112712b
anastasiajsokol <anastasia.j.sokol@gmail.com>
2023-08-27 14:54:02 -0600

    create expression stream file

---
+++ parser/expressionstream.hpp
@@ -0,0 +1,12 @@
+/**
+ *      @file parser/expressionstream.hpp
+ *      @brief defines template class ExpressionStream in namespace parser for turning a generic container stream of blocks into a stream of expressions
+ *      @author Anastasia Sokol
+**/
+
+#ifndef PARSER_EXPRESSIONSTREAM_H
+#define PARSER_EXPRESSIONSTREAM_H
+
+
+
+#endif
