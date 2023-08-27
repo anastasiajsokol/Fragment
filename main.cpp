0560b29af5232d57074e3dea6a0400cfe49633c4
anastasiajsokol <anastasia.j.sokol@gmail.com>
2023-08-27 14:36:26 -0600

    show full progress test so far

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
+#include "lexer/invalidlexeme.hpp"
 #include "lexer/lexstream.hpp"
 #include "parser/blockstream.hpp"
+#include "parser/invalidblock.hpp"
 
 #include <cstdio>
 
@@ -10,6 +12,10 @@ int main(){
         for(const Block& block : parser::BlockStream(lexer::LexStream(filepath))){
             std::printf("Block with size: %ld\n", block.size());
         }
+    } catch(std::ios_base::failure error){
+        std::printf("File Error\n\t%s\n", error.what());
+    } catch(lexer::InvalidLexeme error) {
+        std::printf("Invalid Lexeme Exception\n\terror: %s\n\tposition: (%ld, %ld) in file %s\n", error.what(), error.position.line, error.position.index, filepath);
     } catch(parser::InvalidBlock error) {
         std::printf("Invalid Block Exception\n\terror: %s\n\tposition: (%ld, %ld) in file %s\n", error.what(), error.position.line, error.position.index, filepath);
     }
