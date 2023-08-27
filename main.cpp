ccb053e68928d89a206917db6a87b251e0f3b734
anastasiajsokol <anastasia.j.sokol@gmail.com>
2023-08-27 14:13:58 -0600

    test blockstream

--- main.cpp
+++ main.cpp
@@ -4,8 +4,14 @@
 #include <cstdio>
 
 int main(){
-    for(const Block& token : parser::BlockStream(lexer::LexStream("test/example_factorial.fl"))){
-        std::puts("yippee");    
+    const char* filepath = "test/example_factorial.fl";
+
+    try {
+        for(const Block& block : parser::BlockStream(lexer::LexStream(filepath))){
+            std::printf("Block with size: %ld\n", block.size());
+        }
+    } catch(parser::InvalidBlock error) {
+        std::printf("Invalid Block Exception\n\terror: %s\n\tposition: (%ld, %ld) in file %s\n", error.what(), error.position.line, error.position.index, filepath);
     }
 
     return 0;
