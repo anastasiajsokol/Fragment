3615caa7fd689ebf05d9fcfac403ff691ab05551
anastasiajsokol <anastasia.j.sokol@gmail.com>
2023-08-29 22:25:10 -0600

    match header type changes

--- value/value.cpp
+++ value/value.cpp
@@ -39,11 +39,11 @@ Value::value_t Value::operator <=(const value_t&) const noexcept(false) {
     throw NotImplemented("Comparison is not implemented for void type");
 }
 
-Value::value_t Value::operator |(const value_t&) const noexcept(false) {
+Value::value_t Value::operator ||(const value_t&) const noexcept(false) {
     throw NotImplemented("Comparison is not implemented for void type");
 }
 
-Value::value_t Value::operator &(const value_t&) const noexcept(false) {
+Value::value_t Value::operator &&(const value_t&) const noexcept(false) {
     throw NotImplemented("Comparison is not implemented for void type");
 }
 
@@ -51,10 +51,10 @@ Value::value_t Value::operator !() const noexcept(false) {
     throw NotImplemented("Negation is not implemented for void type");
 }
 
-Value::operator std::string(){
+Value::operator std::string() const {
     throw NotImplemented("Casting to std::string is not implemented for void type");
 }
 
-Value::operator bool(){
+Value::operator bool() const {
     throw NotImplemented("Casting to boolean is not implemented for void type");
 }
