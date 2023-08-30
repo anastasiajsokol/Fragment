15e728a915f6ebe1f001da6e21c0b2415e930260
anastasiajsokol <anastasia.j.sokol@gmail.com>
2023-08-29 20:50:47 -0600

    add description

--- value/valuetype.h
+++ value/valuetype.h
@@ -1,3 +1,9 @@
+/**
+ *      @file value/valuetype.h
+ *      @brief defines ValueType which is used to keep track of type of Value objects
+ *      @author Anastasia Sokol
+**/
+
 #ifndef VALUE_VALUETYPE_H
 #define VALUE_VALUETYPE_H
 
