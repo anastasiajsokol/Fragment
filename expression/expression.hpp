2c48687e524769f076d3a8ef9487c07541b97510
anastasiajsokol <anastasia.j.sokol@gmail.com>
2023-08-31 09:02:59 -0600

    redefine expression interface

--- expression/expression.hpp
+++ expression/expression.hpp
@@ -7,13 +7,18 @@
 #ifndef DATATYPE_EXPRESSION_H
 #define DATATYPE_EXPRESSION_H
 
-#include "../value/value.hpp"       // defines Value which is used to represent a loosely typed value of any of Fragment Lisp's base value types
-#include "../datatype/token.hpp"    // defines Token::TokenPosition used to represent starting position of expression in the file
+#include "../value/value.hpp"           // defines Value which is used to represent a loosely typed value of any of Fragment Lisp's base value types
+#include "../datatype/token.hpp"        // defines Token::TokenPosition used to represent starting position of expression in the file
+#include "../datatype/programstate.h"   // defines ProgramState for adding state to otherwise stateless expressions
+
+#include <memory>   // defines std::unqiue_ptr for managing expressions
 
 /**
  *  @brief represents a code expression
 **/
 struct Expression {
+    typedef std::unique_ptr<Expression> expression_t;   // allows for calls to overloaded functions in a memory safe way
+
     Token::TokenPosition position;  // stores file position of first token in expression
 
     /**
@@ -26,7 +31,7 @@ struct Expression {
      *  @brief enforces that all expression subclasses are evaluatable and that operator () can be called without knowing the specific subtype
      *  @return value representing the value of the expression (note that not all expressions are pure)
     **/
-    virtual Value operator ()() = 0;
+    virtual Value::value_t operator ()(ProgramState&) = 0;
 };
 
 #endif
