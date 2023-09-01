a6c13a9d07a6b215d7d793201f767999c61b8cdd
anastasiajsokol <anastasia.j.sokol@gmail.com>
2023-08-31 21:06:37 -0600

    add to_string(ValueType)

--- value/valuetype.h
+++ value/valuetype.h
@@ -7,11 +7,15 @@
 #ifndef VALUE_VALUETYPE_H
 #define VALUE_VALUETYPE_H
 
+#include <string>   // defines std::string
+
 enum class ValueType {
     numeric,
     string,
     boolean,
     function
 };
+
+std::string to_string(ValueType type);
 
 #endif
