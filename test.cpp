dd3ccd7b3ff43517d2440b58977b0e7410b4e381
anastasiajsokol <anastasia.j.sokol@gmail.com>
2023-09-01 07:37:21 -0600

    semi unrelated but... here

--- test.cpp
+++ test.cpp
@@ -3,10 +3,28 @@
 #include <iterator>
 #include <vector>
  
+template <typename iter_t>
+void bubble(iter_t begin, iter_t end){
+    for(iter_t i = begin; i < end; ++i){
+        for(iter_t j = begin; j < end - (i - begin); ++j){
+            iter_t n = j + 1;
+            if(*j > *n){
+                std::iter_swap(j, n);
+            }
+        }
+    }
+}
+
 int main() {
-    std::string s = "Hello World";
-    std::fill_n(std::back_inserter(s), 3, '!');
-    for (const auto& n : s)
-        std::cout << n << ' ';
-    std::cout << '\n';
+    int values[] = {4, 2, 3, 1};
+
+    bubble(values, values + sizeof(values) / sizeof(int));
+
+    for(size_t i = 0; i < sizeof(values) / sizeof(int); ++i){
+        std::cout << values[i] << " ";
+    }
+
+    std::cout << std::endl;
+
+    return EXIT_SUCCESS;
 }
