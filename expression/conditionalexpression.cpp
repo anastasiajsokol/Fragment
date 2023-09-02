54ea7ddefb929458396d0d2897ac44e460b191ed
anastasiajsokol <anastasia.j.sokol@gmail.com>
2023-09-01 18:09:56 -0600

    fix boolean conversion

--- expression/conditionalexpression.cpp
+++ expression/conditionalexpression.cpp
@@ -3,7 +3,7 @@
 ConditionalExpression::ConditionalExpression(const Token::TokenPosition &position, Expression::expression_t condition, Expression::expression_t truthy, Expression::expression_t falsy) : Expression(position), condition(std::move(condition)), truthy(std::move(truthy)), falsy(std::move(falsy)) {}
 
 Value::value_t ConditionalExpression::operator ()(ProgramState& state) const {
-    if((bool)((*condition)(state))){
+    if((bool)*((*condition)(state))){
         return (*truthy)(state);
     } else {
         return (*falsy)(state);
